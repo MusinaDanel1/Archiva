cba7a38a51a3e91b8f0e97fa0e1c271276a35971
dmussina <dmussina@gmail.com>
2024-11-19 11:46:58 +0500

    create_archive

--- internal/services/create_archive.go
+++ internal/services/create_archive.go
@@ -1,107 +1,39 @@
 package services
 
 import (
-	"archiva/internal/models"
 	"archive/zip"
 	"bytes"
-	"errors"
+	"fmt"
 	"io"
 	"mime/multipart"
-	"net/http"
 )
 
-var allowedMimeTypes = map[string]bool{
-	"application/vnd.openxmlformats-officedocument.wordprocessingml.document": true,
-	"application/xml": true,
-	"image/jpeg":      true,
-	"image/png":       true,
-}
-
-// ProcessFiles обрабатывает файлы, проверяет их типы и создаёт ZIP-архив.
-func ProcessFiles(files []*multipart.FileHeader) (*models.Archive, error) {
-	var zipBuffer bytes.Buffer
-	zipWriter := zip.NewWriter(&zipBuffer)
-
-	var totalSize float64
-	var archiveFiles []models.File
+func ProcessFiles(files []*multipart.FileHeader) ([]byte, error) {
+	var buf bytes.Buffer
+	zipWriter := zip.NewWriter(&buf)
 
 	for _, fileHeader := range files {
-		// Открываем файл
 		file, err := fileHeader.Open()
 		if err != nil {
-			return nil, err
+			return nil, fmt.Errorf("unable to open file %s: %v", fileHeader.Filename, err)
 		}
 		defer file.Close()
 
-		// Проверяем MIME тип
-		mimeType, err := validateFileMimeType(file)
-		if err != nil {
-			return nil, err
-		}
-
-		// Записываем файл в архив
 		zipFile, err := zipWriter.Create(fileHeader.Filename)
 		if err != nil {
-			return nil, err
+			return nil, fmt.Errorf("unable to create zip file %s: %v", fileHeader.Filename, err)
 		}
 
-		// Копируем содержимое файла в архив
 		_, err = io.Copy(zipFile, file)
 		if err != nil {
-			return nil, err
+			return nil, fmt.Errorf("unable to copy file %s to zip: %v", fileHeader.Filename, err)
 		}
-
-		// Добавляем информацию о файле в список
-		archiveFiles = append(archiveFiles, models.File{
-			FilePath: fileHeader.Filename,
-			Size:     float64(fileHeader.Size),
-			Mimetype: mimeType,
-		})
-
-		// Накапливаем общий размер архива
-		totalSize += float64(fileHeader.Size)
 	}
 
-	// Закрываем архивный файл
 	err := zipWriter.Close()
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("unable to close zip archive: %v", err)
 	}
 
-	// Создаём объект архива
-	archive := &models.Archive{
-		Filename:    "archive.zip", // или можно передать имя в параметре
-		ArchiveSize: float64(zipBuffer.Len()),
-		TotalSize:   totalSize,
-		TotalFiles:  float64(len(archiveFiles)),
-		Files:       archiveFiles,
-	}
-
-	return archive, nil
-}
-
-// validateFileMimeType проверяет MIME тип файла
-func validateFileMimeType(file multipart.File) (string, error) {
-	// Read the first 512 bytes to detect MIME type
-	buffer := make([]byte, 512)
-	_, err := file.Read(buffer)
-	if err != nil {
-		return "", errors.New("unable to read file for MIME type detection")
-	}
-
-	// Use http.DetectContentType to get the MIME type
-	mimeType := http.DetectContentType(buffer)
-
-	// If the MIME type is not valid, return an error
-	if !allowedMimeTypes[mimeType] {
-		return "", errors.New("unsupported MIME type: " + mimeType)
-	}
-
-	// Reset the file pointer back to the start after reading the buffer
-	_, err = file.Seek(0, io.SeekStart)
-	if err != nil {
-		return "", errors.New("unable to reset file pointer")
-	}
-
-	return mimeType, nil
+	return buf.Bytes(), nil
 }
